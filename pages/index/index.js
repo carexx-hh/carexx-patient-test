4cc5a4022f2fe3a4213da0aa30e73be7bf890db9
zhouleizl <799221247@qq.com>
2018-10-23 10:59:52 +0800

    11

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Carexx',
+    motto: 'Carexx-Applet',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
