f623811b7a18408052631a58c507d6e2699542f0
jianghainan <3386711609@qq.com>
2019-04-29 11:49:47 +0800

    提现请求中，点击提现直接返回

--- pages/withdraw-deposit/withdraw-deposit.js
+++ pages/withdraw-deposit/withdraw-deposit.js
@@ -7,8 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-      money:'',
-      payAmt:''
+      money:'',  //余额
+      payAmt:'',  //提现金额
+      canClick:true,  //可以点击
   },
 
   /**
@@ -35,92 +36,104 @@ Page({
    * 提现
    */
   
-  withdraw(){
-      let payAmt= this.data.payAmt;
-      let money= this.data.money;
-      let openId,token;
-      if(!payAmt){
-          wx.showToast({
-              title: '请输入提现金额',
-              image: './images/cancel.png',
-              duration: 2000
-          });
-          return;
-            
-      }else if(payAmt>money){
-          wx.showToast({
-              title: '账户余额不足',
-              image: './images/cancel.png',
-              duration: 2000
-          });
-          return;
-      }
-      console.log(typeof payAmt)
-      console.log(payAmt !== '' && payAmt <= money)
-      if (!(payAmt !== '' && payAmt <= money)){
-          wx.showToast({
-              title: '请输入有效金额',
-          });
-          return;
-            
-      }
+    withdraw(){
+        if(!this.data.canClick) return false;
+        let payAmt= this.data.payAmt;
+        let money= this.data.money;
+        let openId,token;
+        if(!payAmt){
+            wx.showToast({
+                title: '请输入提现金额',
+                image: './images/cancel.png',
+                duration: 2000
+            });
+            return;
+                
+        }else if(payAmt>money){
+            wx.showToast({
+                title: '账户余额不足',
+                image: './images/cancel.png',
+                duration: 2000
+            });
+            return;
+        }
+        console.log(typeof payAmt)
+        console.log(payAmt !== '' && payAmt <= money)
+        if (!(payAmt !== '' && payAmt <= money)){
+            wx.showToast({
+                title: '请输入有效金额',
+            });
+            return;
+                
+        }
 
-      try {
-          openId = wx.getStorageSync('openId')
-          token = wx.getStorageSync('token')
-          if (!openId) {
-              // Do something with return value
-              wx.showToast({
-                  title: 'openId不存在'
-              });
-              return;
-                
-          }
-          if (!token) {
-              // Do something with return value
-              wx.showToast({
-                  title: 'token不存在'
-              });
-              return;
-                
-          }
-      } catch (e) {
-          // Do something when catch error
-          console.log(e)
-      }
-      wx.request({
-        url: app.globalData.baseUrl + '/useraccount/refund',
-          data: {
-              openId,
-              payAmt
-          },
-          header: {
-              'content-type':'application/x-www-form-urlencoded',
-              'auth-token':token
-              },
-          method: 'post',
-          dataType: 'json',
-          responseType: 'text',
-          success: (res)=>{
-              console.log(res)
-              if(res.data.code==200)
-              wx.redirectTo({
-                  url: '/pages/withdraw-deposit-result/withdraw-deposit-result',
-              });
-              else wx.showToast({
-                  title: 'err,code:'+res.data.code,
-                  image: './images/cancel.png',
-                  duration: 2000
-              });
-          },
-          fail: ()=>{
-              wx.showToast({
-                  title: '提现失败，请稍候再试'
-              });
-          },
-          complete: ()=>{}
-      });
-  },
+        try {
+            openId = wx.getStorageSync('openId')
+            token = wx.getStorageSync('token')
+            if (!openId) {
+                // Do something with return value
+                wx.showToast({
+                    title: 'openId不存在'
+                });
+                return;
+                    
+            }
+            if (!token) {
+                // Do something with return value
+                wx.showToast({
+                    title: 'token不存在'
+                });
+                return;
+                    
+            }
+        } catch (e) {
+            // Do something when catch error
+            console.log(e)
+        }
+        let that = this;
+        that.data.canClick=false;
+        wx.request({
+            url: app.globalData.baseUrl + '/useraccount/refund',
+            data: {
+                openId,
+                payAmt
+            },
+            header: {
+                'content-type':'application/x-www-form-urlencoded',
+                'auth-token':token
+                },
+            method: 'post',
+            dataType: 'json',
+            responseType: 'text',
+            success: (res)=>{
+                console.log(res)
+                if(res.data.code==200)
+                    wx.redirectTo({
+                        url: '/pages/withdraw-deposit-result/withdraw-deposit-result',
+                    });
+                else {
+                    wx.showToast({
+                        title: 'err,code:'+res.data.code,
+                        image: './images/cancel.png',
+                        duration: 2000,
+                        success: function (params) {
+                            that.data.canClick = true;
+                        }
+                    });
+                }
+            },
+            fail: ()=>{
+                wx.showToast({
+                    title: '提现失败，请稍候再试',
+                    success:function (params) {
+                        that.data.canClick=true;
+                    }
+                });
+            },
+            complete: ()=>{
+            }
+        });
+    },
     /**
      * 全部提现
      */
